697aeb22b7aa209283630931b91dd3f02b206eee
ggichuru <ggichuru.dev@gmail.com>
2020-11-25 05:39:32 +0300

    User routing fix

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from '../common/guard/auth.guard';
 import { UserGuard } from './guard/user.guard';
+import { ProductDetailsComponent } from './product/product-details/product-details.component';
+import { ProductRoutingModule } from './product/product-routing.module';
+import { ProductComponent } from './product/product.component';
 import { ProfileEditComponent } from './profile/profile-edit/profile-edit.component';
 import { ProfileComponent } from './profile/profile.component';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
@@ -13,7 +16,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [
+    RouterModule.forChild(routes),
+    ProductRoutingModule
+  ],
   exports: [RouterModule]
 })
 export class UserRoutingModule { }
